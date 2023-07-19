c9ec0afabb6e88eb6dc3b512923eb241a21d594b
Bommu Madhusudhanreddy <131128006+Madhu125309@users.noreply.github.com>
2023-07-19 16:41:24 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
-import Message from "./Message"
-function App(){
-    return <div>
-        <h1>App development</h1>
-        <Message />
+import React from 'react'
+import Employee from './Employee'
+function App() {
+  return (
+    <div>
+    <h1>Employee</h1>
+    <h2>Movie</h2>
+    <h3>Course</h3>
+    <Employee/>
     </div>
+
+  )
 }
+
 export default App
